730ae908eb5b6e65bd408a78eca66670e891be91
Naren Sulegai <86naren@gmail.com>
2021-04-01 17:25:15 -0700

    Auth0 login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from "./components/LoginButton";
+import Profile from "./components/Profile";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <LoginButton/>
+                <LogoutButton/>
+            </div>
+            <div>
+                <Profile/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Auth0Provider
+            domain="cbf.auth0.com"
+            clientId="tgnBPUcKrWSRBs2P5SFLfo104gRWmuNO"
+            redirectUri={window.location.origin}
+        >
+            <App/>
+        </Auth0Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
