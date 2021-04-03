03678be2a6da65dccd61796ed656af7033d44486
Naren Sulegai <86naren@gmail.com>
2021-04-02 22:21:41 -0700

    Send message

--- src/App.js
+++ src/App.js
@@ -13,38 +13,53 @@ import Events from "./components/Events";
 import PhotoGallery from "./components/PhotoGallery";
 import Donate from "./components/Donate";
 import ContactUs from "./components/ContactUs";
+import {createMuiTheme} from "@material-ui/core";
+import {ThemeProvider} from '@material-ui/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#7d2ae8',
+        },
+        secondary: {
+            main: '#c496ff',
+        },
+    },
+});
 
 export default function App() {
     return (
-        <Router>
-            <Layout>
-                <Switch>
-                    <Route exact path="/">
-                        <Home/>
-                    </Route>
-                    <Route path="/home">
-                        <Home/>
-                    </Route>
-                    <Route path="/whoarewe">
-                        <Whoarewe/>
-                    </Route>
-                    <Route path="/monthlynewsletter">
-                        <MonthlyNewsLetter/>
-                    </Route>
-                    <Route path="/events">
-                        <Events/>
-                    </Route>
-                    <Route path="/photogallery">
-                        <PhotoGallery/>
-                    </Route>
-                    <Route path="/donate">
-                        <Donate/>
-                    </Route>
-                    <Route path="/contactus">
-                        <ContactUs/>
-                    </Route>
-                </Switch>
-            </Layout>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                        <Route path="/home">
+                            <Home/>
+                        </Route>
+                        <Route path="/whoarewe">
+                            <Whoarewe/>
+                        </Route>
+                        <Route path="/monthlynewsletter">
+                            <MonthlyNewsLetter/>
+                        </Route>
+                        <Route path="/events">
+                            <Events/>
+                        </Route>
+                        <Route path="/photogallery">
+                            <PhotoGallery/>
+                        </Route>
+                        <Route path="/donate">
+                            <Donate/>
+                        </Route>
+                        <Route path="/contactus">
+                            <ContactUs/>
+                        </Route>
+                    </Switch>
+                </Layout>
+            </Router>
+        </ThemeProvider>
     );
 }
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const ContactUs = props => {
+
+    const [message, setMessage] = useState("");
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+    }
+
     return (
         <div>
-            ContactUs
+            <h2>Contact Us</h2>
+            <div className="small-margin-top">
+                <TextField
+                    label="Student name"
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth="true"
+                />
+                <TextField
+                    label="Email"
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth="true"
+                />
+            </div>
+
+            <div className="small-margin-top">
+                <TextField
+                    label="Message"
+                    multiline
+                    rows={4}
+                    value={message}
+                    onChange={handleMessageChange}
+                    variant="outlined"
+                    fullWidth="true"
+                />
+            </div>
+            <div className="small-margin-top center">
+                <div className="button">Send</div>
+            </div>
         </div>
     );
 };
 
-ContactUs.propTypes = {
-
-};
+ContactUs.propTypes = {};
 
 export default ContactUs;
